cb975f5c570c8a3d419885c1673547c4c3facb4d
Karthikeya <veruturikarthikeya@gmail.com>
2024-10-16 18:47:09 +0530

    promotion mails

---
+++ mails.js
@@ -0,0 +1,14 @@
+const mails = [
+    "maneeshkumar1728@gmail.com",
+    
+]
+
+
+// const mails = [
+//   "goudmvardhan@gmail.com",
+//   "parshipraneesh8@gmail.com",
+//   "bandibindusree@gmail.com",
+//   "nallapunenisaiyudhi@gmail.com",
+//   "sathvik7593@gmail.com",
+// ];
+export { mails };
