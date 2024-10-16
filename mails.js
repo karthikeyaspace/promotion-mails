0f0dd7be467ed909ee5d757973f80733b997cd6a
Karthikeya <veruturikarthikeya@gmail.com>
2024-10-16 20:06:41 +0530

    final template

--- mails.js
+++ mails.js
@@ -1,14 +1,18 @@
-const mails = [
-    "maneeshkumar1728@gmail.com",
-    
-]
-
-
 // const mails = [
+//   "karthikeyaveruturi2005@gmail.com",
 //   "goudmvardhan@gmail.com",
-//   "parshipraneesh8@gmail.com",
 //   "bandibindusree@gmail.com",
+//   "parshipraneesh8@gmail.com",
+//   "vaishamsini@gmail.com",
+//   "saicharan73966@gmail.com",
+//   "maneeshkumar1728@gmail.com",
+//   "akhileshwaranumula2509@gmail.com",
 //   "nallapunenisaiyudhi@gmail.com",
+//   "snigdhakandula23@gmail.com",
+//   "avisurabhi21@gmail.com ",
 //   "sathvik7593@gmail.com",
+// "adithyakadukuntla@gmail.com"
 // ];
+
+
 export { mails };
